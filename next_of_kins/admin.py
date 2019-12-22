ba612b140a61ad3ed63019f998204b37da56129d
Timothy Karani <kartimothy@gmail.com>
2019-12-22 17:41:10 +0300

    Added the next of kin model

--- next_of_kins/admin.py
+++ next_of_kins/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import NextOfKin, KinPrisoner
 
-# Register your models here.
+class NextOfKinAdmin(admin.ModelAdmin):
+    model = NextOfKin
+    list_display = ['user',]
+
+class KinPrisonerAdmin(admin.ModelAdmin):
+    model = KinPrisoner
+    list_display = ['kin', 'prisoner',]
+
+admin.site.register(NextOfKin, NextOfKinAdmin)
+admin.site.register(KinPrisoner, KinPrisonerAdmin)
