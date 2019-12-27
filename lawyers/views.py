ff57894a3ccb9e7887b8ca1a3fb32663e820f314
Timothy Karani <kartimothy@gmail.com>
2019-12-27 06:10:51 +0300

    Restricted access to Lawyer views

--- lawyers/views.py
+++ lawyers/views.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin
+)
 from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, DeleteView
@@ -5,19 +9,29 @@ from django.urls import reverse_lazy
 
 from .models import Lawyer, LawyerClient
 
-class LawyersDetailView(DetailView):
+class LawyersDetailView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
     model = Lawyer
     template_name = 'lawyers/detail.html'
+    login_url = 'login'
 
-class LawyersCreateView(CreateView):
+    def test_func(self):
+        is_lawyer = Lawyer.objects.filter(user=self.request.user).exists()
+        return self.request.user.is_superuser or is_lawyer
+
+class LawyersCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Lawyer
     fields = ('user',)
     template_name = 'lawyers/new.html'
+    login_url = 'login'
 
-class LawyerClientCreateView(CreateView):
+    def test_func(self):
+        return self.request.user.is_superuser
+
+class LawyerClientCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = LawyerClient
     fields = ('prisoner',)
     template_name = 'lawyerclients/new.html'
+    login_url = 'login'
 
     # def get_initial(self):
         # initial = super(LawyerClientCreateView, self).get_initial()
@@ -36,11 +50,25 @@ class LawyerClientCreateView(CreateView):
         form.instance.lawyer = lawyer
         return super(LawyerClientCreateView, self).form_valid(form)
 
-class LawyerClientListView(ListView):
+    def test_func(self):
+        is_lawyer = Lawyer.objects.filter(user=self.request.user).exists()
+        return self.request.user.is_superuser or is_lawyer
+
+class LawyerClientListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
     model = LawyerClient
     template_name = 'lawyerclients/list.html'
+    login_url = 'login'
 
-class LawyerClientDeleteView(DeleteView):
+    def test_func(self):
+        is_lawyer = Lawyer.objects.filter(user=self.request.user).exists()
+        return self.request.user.is_superuser or is_lawyer
+
+class LawyerClientDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = LawyerClient
     template_name = 'lawyerclients/delete.html'
     success_url = reverse_lazy('lawyerclients_list')
+    login_url = 'login'
+
+    def test_func(self):
+        is_lawyer = Lawyer.objects.filter(user=self.request.user).exists()
+        return self.request.user.is_superuser or is_lawyer
