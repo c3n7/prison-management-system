cb3d1ed17b3d3423026979ed55d1e1c0cf48ac49
Timothy Karani <kartimothy@gmail.com>
2019-12-27 05:44:35 +0300

    Restricted access to prisoners views

--- prisoners/views.py
+++ prisoners/views.py
@@ -1,23 +1,40 @@
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin
+)
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView
 
 from .models import Prisoner
 
-class PrisonersListView(ListView):
+class PrisonersListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
     model = Prisoner
     ordering = ['id_number', 'first_name', 'last_name']
     template_name = 'prisoners/list.html'
+    login_url = 'login'
 
-class PrisonersDetailView(DetailView):
+    def test_func(self):
+        return self.request.user.is_superuser
+
+class PrisonersDetailView(LoginRequiredMixin, DetailView):
     model = Prisoner
     template_name = 'prisoners/detail.html'
+    login_url = 'login'
 
-class PrisonersUpdateView(UpdateView):
+class PrisonersUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Prisoner
     fields = ('id_number', 'first_name', 'last_name')
     template_name = 'prisoners/edit.html'
+    login_url = 'login'
 
-class PrisonersCreateView(CreateView):
+    def test_func(self):
+        return self.request.user.is_superuser
+
+class PrisonersCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Prisoner
     fields = ('id_number', 'first_name', 'last_name')
     template_name = 'prisoners/new.html'
+    login_url = 'login'
+
+    def test_func(self):
+        return self.request.user.is_superuser
