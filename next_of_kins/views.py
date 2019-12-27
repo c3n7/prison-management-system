e8f748bb778e395b3e323b8a92d206ca2f0bc89a
Timothy Karani <kartimothy@gmail.com>
2019-12-27 06:38:34 +0300

    Restricted access to the nextofkin views

--- next_of_kins/views.py
+++ next_of_kins/views.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin
+)
 from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -5,16 +9,23 @@ from django.urls import reverse_lazy
 
 from .models import NextOfKin, KinPrisoner
 
-class NextOfKinDetailView(DetailView):
+class NextOfKinDetailView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
     model = NextOfKin
     template_name = 'next_of_kins/detail.html'
 
-class NextOfKinCreateView(CreateView):
+    def test_func(self):
+        is_kin = NextOfKin.objects.filter(user=self.request.user).exists()
+        return self.request.user.is_superuser or is_kin
+
+class NextOfKinCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = NextOfKin
     fields = ('user',)
     template_name = 'next_of_kins/new.html'
 
-class KinPrisonerCreateView(CreateView):
+    def test_func(self):
+        return self.request.user.is_superuser
+
+class KinPrisonerCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = KinPrisoner
     fields = ('prisoner', 'relationship',)
     template_name = 'kinprisoners/new.html'
@@ -29,11 +40,23 @@ class KinPrisonerCreateView(CreateView):
         form.instance.kin = kin
         return super(KinPrisonerCreateView, self).form_valid(form)
 
-class KinPrisonerListView(ListView):
+    def test_func(self):
+        is_kin = NextOfKin.objects.filter(user=self.request.user).exists()
+        return self.request.user.is_superuser or is_kin
+
+class KinPrisonerListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
     model = KinPrisoner
     template_name = 'kinprisoners/list.html'
 
-class KinPrisonerDeleteView(DeleteView):
+    def test_func(self):
+        is_kin = NextOfKin.objects.filter(user=self.request.user).exists()
+        return self.request.user.is_superuser or is_kin
+
+class KinPrisonerDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = KinPrisoner
     template_name = 'kinprisoners/delete.html'
     success_url = reverse_lazy('kinprisoners_list')
+
+    def test_func(self):
+        is_kin = NextOfKin.objects.filter(user=self.request.user).exists()
+        return self.request.user.is_superuser or is_kin
